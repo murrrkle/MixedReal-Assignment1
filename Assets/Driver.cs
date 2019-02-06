28c4ccd0a8f3d13cd5fdd34d3c911310346308fb
Michael Hung <myshung@ucalgary.ca>
2019-02-06 11:45:29 -0700

    first

---
+++ Assets/Driver.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    public List<GameObject> checkpoints;
+    int state; // 0 = idle, 1 = moving
+    public int cardsDetected;
+    int nextCheckpoint;
+
+    static int idleState = Animator.StringToHash("Base Layer.Idle");
+    static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+    static int jumpState = Animator.StringToHash("Base Layer.Jump");
+    static int restState = Animator.StringToHash("Base Layer.Rest");
+    private CapsuleCollider col;
+    private Rigidbody rb;
+    private AudioSource asce;
+    private Animator anim;                          
+    private AnimatorStateInfo currentBaseState;
+    private float orgColHight;
+    private Vector3 orgVectColCenter;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = 0;
+        cardsDetected = 0;
+        nextCheckpoint = 0;
+        
+        anim = GetComponent<Animator>();
+        col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+
+        orgColHight = col.height;
+        orgVectColCenter = col.center;
+        asce = GetComponent<AudioSource>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (cardsDetected >= 3)
+        {
+            state = 1;
+        } else
+        {
+            state = 0;
+        }
+
+        if (state == 1)
+        {
+            if (nextCheckpoint <=3)
+            {
+                gameObject.transform.LookAt(checkpoints[nextCheckpoint].transform);
+                anim.SetBool("Locomotion", true);
+                if (rb.velocity.magnitude <= 0)
+                {
+                    this.rb.AddForce(Vector3.Normalize(this.transform.forward)*1.8f, ForceMode.Impulse);
+                }
+                anim.SetBool("Jump", false);
+
+            } else
+            {
+                anim.SetBool("Locomotion", false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "jump")
+        {
+            anim.SetBool("Jump", true);
+        }
+        else if (other.tag == "Finish")
+        {
+            asce.Play();
+            GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
---
+++ Assets/EasyAR/Scripts/ImageTargetBehaviour.cs
@@ -0,0 +1,47 @@
+//=============================================================================================================================
+//
+// Copyright (c) 2015-2017 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+// EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+// and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//=============================================================================================================================
+
+using UnityEngine;
+
+namespace EasyAR
+{
+    public class ImageTargetBehaviour : ImageTargetBaseBehaviour
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            TargetFound += OnTargetFound;
+            TargetLost += OnTargetLost;
+            TargetLoad += OnTargetLoad;
+            TargetUnload += OnTargetUnload;
+        }
+        void OnTargetFound(TargetAbstractBehaviour behaviour)
+        {
+            Driver player = GameObject.FindGameObjectWithTag("Player").GetComponent<Driver>();
+            player.cardsDetected += 1;
+            Debug.Log("Found: " + Target.Id);
+        }
+
+        void OnTargetLost(TargetAbstractBehaviour behaviour)
+        {
+            Driver player = GameObject.FindGameObjectWithTag("Player").GetComponent<Driver>();
+            player.cardsDetected -= 1;
+            Debug.Log("Lost: " + Target.Id);
+        }
+
+        void OnTargetLoad(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+        {
+            Debug.Log("Load target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+        }
+
+        void OnTargetUnload(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+        {
+            Debug.Log("Unload target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+        }
+    }
+}
